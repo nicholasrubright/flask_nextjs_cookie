8d3b4690ad84120d7194c28869d18b19a6bc96a9
Nicholas <koalrube@gmail.com>
2023-09-14 17:50:52 -0400

    changed how cookie

--- client/src/app/api/movies/route.ts
+++ client/src/app/api/movies/route.ts
@@ -27,12 +27,13 @@ export async function GET(request: NextRequest) {
   // console.log("newSetCookies: ", newSetCookies);
 
   responseCookies.forEach((cookie) => {
-    cookies().set(cookie.name, cookie.value);
+    //cookies().set(cookie.name, cookie.value);
+    response.headers.set("Set-Cookie", `${cookie.name}=${cookie.value}`);
   });
 
-  console.log("responseCookies: ", responseCookies);
+  //console.log("responseCookies: ", responseCookies);
 
-  console.log("cookies currently set: ", cookies().getAll());
+  //console.log("cookies currently set: ", cookies().getAll());
 
   // newSetCookies.forEach((cookie) => {
   //   response.headers.set("set-cookie", cookie);
